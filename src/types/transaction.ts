3250ca263b9761fcddd75f67aec463b5fb3751d8
gerschnaider <gersch2002@gmail.com>
2026-05-25 23:46:57 +0000

    refactor: Change in database schema to treat status as a defined type instead of normal string

---
+++ src/types/transaction.ts
@@ -0,0 +1,31 @@
+export type TransactionStatus = "PENDING" | "COMPLETED" | "FAILED" | "REFUNDED" | "CANCELLED";
+
+
+interface BaseTransactionDetails {
+    transaction_id: string;
+    trip_id: string;
+    amount: string | number;
+    status: TransactionStatus;
+    created_at: Date | string;
+    external_id?: string | null;
+}
+
+export interface PaymentDetails extends BaseTransactionDetails {
+    type: "PAYMENT";
+    updated_at: Date | string;
+    expiration_date?: Date | string | null;
+}
+
+export interface DisbursementDetails extends BaseTransactionDetails {
+    type: "DISBURSEMENT";
+    payment_alias: string;
+    platform_fee: string | number;
+}
+
+export interface RefundDetails extends BaseTransactionDetails {
+    type: "REFUND";
+    refund_type: "TOTAL" | "PARTIAL";
+    reason?: string | null; // Agregamos el motivo que viene de la BD
+}
+
+export type TransactionDetailProps = PaymentDetails | DisbursementDetails | RefundDetails;
